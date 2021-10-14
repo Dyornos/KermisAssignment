73f901772555bc44c650b23d8c4473ecd51e617d
Dyornos <joppe_hendriks@hotmail.nl>
2021-10-14 15:03:18 +0200

    Final update

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace Kermis
         {
 
             ConsoleKeyInfo userInput;
+            TaxGuy taxGuy = new TaxGuy();
             Attractie[] attracties = new Attractie[] { new BouncingCars(), new Spin(), new MirrorPalace(), new HauntedHouse(), new Hawaii(), new LadderClimbing() };
             CashRegister cashRegister = new CashRegister(attracties);
             Console.WriteLine("Welcome to the carnival controll panel! Please press the following numbers to visit one of the rides! \n" +
@@ -20,7 +21,9 @@ namespace Kermis
             "3 - MirrorPalace\n" +
             "4 - Haunted House\n" +
             "5 - Hawaii\n" +
-            "6 - Ladder climbing\n\n");
+            "6 - Ladder climbing\n" +
+            "o - See total revenue of the park\n" +
+            "k - See total amount of tickets sold\n\n");
 
 
 
@@ -35,6 +38,15 @@ namespace Kermis
                     case "k":
                         cashRegister.ShowTotalTicketsSold();
                         break;
+                    case "m":
+                        for (int i = 0; i < attracties.Length; i++)
+                        {
+                            if(attracties[i] is HighMaintananceRiskAttraction)
+                            {
+                                ((HighMaintananceRiskAttraction)attracties[i]).Repair();
+                            }
+                        }
+                        break;
                     case "1":
                     case "2":
                     case "3":
@@ -48,6 +60,11 @@ namespace Kermis
                         break;
                 }
 
+                if(cashRegister.CalculateTicketsSold() - cashRegister.taxGuyLastTicketTime >= 15)
+                {
+                    cashRegister.taxGuyLastTicketTime = cashRegister.CalculateTicketsSold();
+                    taxGuy.TakeTax(attracties);
+                }
             } while (userInput.Key != ConsoleKey.Escape);
         }
     }
@@ -67,7 +84,7 @@ namespace Kermis
         private Attractie[] attracties;
         double totalParkRevenue;
         int totalTicketsSold;
-
+        public int taxGuyLastTicketTime = 0;
         private double CalculateTotalParkRevenue()
         {
             totalParkRevenue = 0;
@@ -77,7 +94,7 @@ namespace Kermis
             }
             return totalParkRevenue;
         }
-        private int CalculateTicketsSold()
+        public int CalculateTicketsSold()
         {
             totalTicketsSold = 0;
             foreach (Attractie a in attracties)
@@ -105,14 +122,58 @@ namespace Kermis
 
         public double revenue = 0;
         public int ticketsSold = 0;
-        public void Run()
+        public virtual void Run()
         {
             Console.WriteLine("\n\nThe " + name + " is running!\n");
             revenue += costs;
             ticketsSold++;
         }
     }
+    public abstract class HighMaintananceRiskAttraction : Attractie
+    {
+        public int maxRidesBeforeRepair;
+        public int currentAmountOfRides;
 
+        public void Repair()
+        {
+            currentAmountOfRides = 0;
+            Console.WriteLine("\n\nThe " + name + " is repaired!\n");
+        }
+
+        override public void Run()
+        {
+            if(currentAmountOfRides == maxRidesBeforeRepair)
+            {
+                Console.WriteLine("\n\nThe " + name + " needs to be repaired before running again! Press 'm' to send a mechanic!\n");
+                return;
+            }
+
+            if (currentAmountOfRides > maxRidesBeforeRepair)
+            {
+                throw new InvalidOperationException("\n Error! " + name + " is broken and needs to be repaired first! Press 'm' to send a mechanic!");
+            }
+
+            Console.WriteLine("\n\nThe " + name + " is running!\n");
+            revenue += costs;
+            ticketsSold++;
+            currentAmountOfRides++;
+        }
+
+    }
+    public class TaxGuy
+    {
+        public void TakeTax(Attractie[] a)
+        {
+            foreach(Attractie attractie in a)
+            {
+                if(attractie is GambleAttraction)
+                {
+                    ((GambleAttraction)attractie).PayGambleGameTax();
+                    Console.WriteLine("Yes this is a game attraction");
+                }
+            }
+        }
+    }
     public class BouncingCars : Attractie
     {
         public BouncingCars()
@@ -129,7 +190,7 @@ namespace Kermis
             name = "Spin";
             idNumber = 2;
             costs = 2.25;
-            
+            maxRidesBeforeRepair = 5;
         }
     }
     public class MirrorPalace : Attractie
@@ -150,28 +211,20 @@ namespace Kermis
             costs = 3.20;
         }
     }
-    public class Hawaii : Attractie
+    public class Hawaii : HighMaintananceRiskAttraction
     {
         public Hawaii()
         {
             name = "Hawaii";
             idNumber = 5;
             costs = 2.90;
+            maxRidesBeforeRepair = 10;
         }
     }
 
-    public abstract class HighMaintananceRiskAttraction : Attractie
-    {
-        int maxRidesBeforeRepair;
-        public int currentAmountOfRides;
 
-        void Repair() {
-            currentAmountOfRides = 0;
-        }
-        
-    }
 
-    public class LadderClimbing : Attractie
+    public class LadderClimbing : Attractie, GambleAttraction
     {
         public LadderClimbing()
         {
@@ -179,7 +232,15 @@ namespace Kermis
             idNumber = 6;
             costs = 5.00;
         }
+
+        public double PayGambleGameTax()
+        {
+            double tax = (revenue / 100) * 30;
+            revenue = revenue - tax;
+            return tax;
+        }
     }
+
     public class aBaseClass
     {
         public aBaseClass()
